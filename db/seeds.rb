5d47528e6b21e01e621ddbee6ebeab687095cf7f
Julien Le Mee <julemee@gmail.com>
2022-08-10 15:01:34 -0400

    rails yelp mvp challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  Restaurant.create!(
+    name: "The #{Faker::Games::Zelda.game}",
+    address: Faker::Games::Zelda.location,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
